3137668eeaead151cbcf51c107e5b69d6c93bccd
nick <146023067+ikoeuo@users.noreply.github.com>
2023-12-10 19:20:22 -0500

    App base created

---
+++ CollectionApp.py
@@ -0,0 +1,23 @@
+import Collection
+
+def main():
+    data = Collection.load_data()
+
+    print('1. Create Item \n2. List Item(s) \n3. Update Item \n4.Delete Item')
+    choice = input('What would you like to do (1-4): ')
+
+
+    if choice == 1:
+        pass
+
+    if choice == 2:
+        pass
+
+    if choice == 3:
+        pass
+
+    if choice == 4:
+        pass
+
+    else:
+        print('Invalid selection')
